acd380114166ff8a850afafd1503c0108ac48e9d
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-01-01 14:56:48 +0545

    fix: import cloudinary from nest-cloudinary

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export * from './interfaces';
+export * from './cloudinary.module-definition';
 export * from './cloudinary.module';
+export * from './cloudinary.options';
 export * from './cloudinary.service';
+export * from './interfaces';
